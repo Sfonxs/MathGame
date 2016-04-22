efba4b886adf2e66915eb543050f8f483af7762e
Xander Berkein <xander.berkein.v7629@student.hogent.be>
2016-04-22 11:10:07 +0200

    "Conventies"

--- public/scripts/methgame.js
+++ public/scripts/methgame.js
@@ -212,11 +212,11 @@ var app = {
                 alert("The answer of " + result.foundResult + " is NOT correct.");
             }
         });
-        $('#btnSubmit').on('click', function (e) {
+        $('#btnReset').on('click', function (e) {
             e.preventDefault();
-            this.init();
-        })
-        setHighscore();
+            app.init();
+        });
+        this.compareCurrentHighscoreWithHighscoreInLocalDataAndChangeHighscoreOnScreenAccordingly();
         var lstOuputs = $('#lstOutputs');
         var lstInputs = $('#lstInputs');
         lstInputs.add(lstOuputs).on('mousedown', 'li', function (event) {
@@ -344,7 +344,7 @@ var app = {
         $('#operators').text(opts[0] + " ["+ opts[1].toString()+"]");
     },
     startNextProb: function () {
-        setHighscore();
+        this.compareCurrentHighscoreWithHighscoreInLocalDataAndChangeHighscoreOnScreenAccordingly();
         this.level++;
         var details = Levels[this.level];
         if (details){
@@ -352,18 +352,19 @@ var app = {
         }
         this.currentProb = new MathProb(this.levelDetails);
         this.currentProbToHtml();
-    }
+    },
+    compareCurrentHighscoreWithHighscoreInLocalDataAndChangeHighscoreOnScreenAccordingly:  function(){
+            if(!localStorage.highscore){
+                localStorage.highscore = 0;
+            }
+            else if(localStorage.highscore < app.level){
+                localStorage.highscore = app.level;
+            }
+            $("#highscoreValue").text(localStorage.highscore);
+        }
+
 };
 
-function setHighscore(){
-    if(!localStorage.highscore){
-        localStorage.highscore = 0;
-    }
-    else if(localStorage.highscore < app.level){
-        localStorage.highscore = app.level;
-    }
-    $("#highscoreValue").text(localStorage.highscore);
-}
 
 $(document).ready(function () {
     app.init();
